01396bc6b85043349f8d7b055f4e9aaf254b0818
chenlinzhong <chenlinzhong@bytedance.com>
2025-06-12 16:09:26 +0800

    feat(kvcache): replace EIC with HKPV

--- python/sglang/srt/mem_cache/eic_memory_pool.py
+++ python/sglang/srt/mem_cache/eic_memory_pool.py
@@ -379,7 +379,7 @@ class EICBaseTokenToKVPoolHost:
         return flat_data, masks
 
     def assign_flat_data(self, indices, flat_data):
-        logger.debug(f"Assign flat data indices {indices},shape={flat_data.shape}")
+        logger.debug(f"Assign flat data indices {indices},shape={flat_data.shape},split_dim={self.split_dim}")
         start_time = time.perf_counter()
 
         keys = self._encode_key_exclusive(indices)
